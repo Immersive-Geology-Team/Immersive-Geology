b790659eb9d76bc7ead625959cda4a69467fc2f8
NightmareTwilight <casper.areskoug@icloud.com>
2020-05-28 08:45:06 +0200

    Added Marble

---
+++ src/main/java/com/igteam/immersivegeology/common/materials/stones/MaterialStoneMarble.java
@@ -0,0 +1,112 @@
+package com.igteam.immersivegeology.common.materials.stones;
+
+import com.igteam.immersivegeology.ImmersiveGeology;
+import com.igteam.immersivegeology.api.materials.PeriodicTableElement;
+import com.igteam.immersivegeology.api.materials.PeriodicTableElement.ElementProportion;
+import com.igteam.immersivegeology.api.materials.material_bases.MaterialStoneBase;
+import net.minecraft.item.Rarity;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+/**
+ * Created by JStocke12 on 27-03-2020.
+ */
+public class MaterialStoneMarble extends MaterialStoneBase
+{
+	//Rhyolite is the default minecraft stone, renamed
+	@Override
+	public String getName()
+	{
+		return "marble";
+	}
+
+	@Nonnull
+	@Override
+	public String getModID()
+	{
+		return ImmersiveGeology.MODID;
+	}
+
+	@Override
+	public LinkedHashSet<ElementProportion> getElements()
+	{
+		return new LinkedHashSet<>(Arrays.asList(
+				new ElementProportion(PeriodicTableElement.ALUMINIUM)
+		));
+	}
+
+	@Override
+	public Rarity getRarity()
+	{
+		return Rarity.COMMON;
+	}
+
+	//A totally arbitrary value
+	@Override
+	public int getBoilingPoint()
+	{
+		return 1520+273+300;
+	}
+
+	//An average value
+	@Override
+	public int getMeltingPoint()
+	{
+		return 1520+273;
+	}
+
+	@Override
+	public int getColor(int temperature)
+	{
+		return 0xfffcf0;
+	}
+
+	//Needs to be changed in code for subtypes, such as sheetmetal
+	@Override
+	public float getHardness()
+	{
+		return 1F;
+	}
+
+	@Override
+	public float getMiningResistance()
+	{
+		return 6.0F;
+	}
+
+	@Override
+	public float getBlastResistance()
+	{
+		return 6f;
+	}
+
+	//Copied from Immersive Intelligence (steel has i think 1.65, leaves 0.35)
+	@Override
+	public float getDensity()
+	{
+		return 1;
+	}
+
+	//Stone pickaxe level
+	@Override
+	public int getBlockHarvestLevel()
+	{
+		return 0;
+	}
+
+	@Override
+	public EnumStoneType getStoneType()
+	{
+		return EnumStoneType.METAMORPHIC;
+	}
+
+
+	/*@Nullable
+	@Override
+	public IItemTier getToolTier()
+	{
+		return IGContent.;
+	}*/
+}
