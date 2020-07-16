ee4c10cea8222e1e8afd0d7aaaffe02aadbb8a1d
Pabilo8 <pabilo1.pg@gmail.com>
2020-07-16 18:27:49 +0200

    Added recipes

--- src/main/java/com/igteam/immersivegeology/common/data/IGRecipeProvider.java
+++ src/main/java/com/igteam/immersivegeology/common/data/IGRecipeProvider.java
@@ -5,6 +5,7 @@ import com.igteam.immersivegeology.common.IGContent;
 import com.igteam.immersivegeology.common.IGRegistryGrabber;
 import com.igteam.immersivegeology.common.blocks.IGBaseBlock;
 import com.igteam.immersivegeology.common.items.IGMaterialResourceItem;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
@@ -16,49 +17,53 @@ import java.util.function.Consumer;
 public class IGRecipeProvider extends RecipeProvider
 {
 
-    public IGRecipeProvider(DataGenerator generatorIn)
-    {
-        super(generatorIn);
-    }
+	public IGRecipeProvider(DataGenerator generatorIn)
+	{
+		super(generatorIn);
+	}
 
-    @Override
-    protected void registerRecipes(Consumer<IFinishedRecipe> consumer)
-    {
-        createBasicRecipes(consumer);
-    }
+	@Override
+	protected void registerRecipes(Consumer<IFinishedRecipe> consumer)
+	{
+		createBasicRecipes(consumer);
+	}
 
-    public void createBasicRecipes(Consumer<IFinishedRecipe> consumer)
-    {
-        for(Item item : IGContent.registeredIGItems.values())
-        {
-            try {
-                if (item instanceof IGMaterialResourceItem) {
-                    IGMaterialResourceItem resourceItem = (IGMaterialResourceItem) item;
-                    if(resourceItem.subtype.equals(MaterialUseType.CHUNK))
-                    {
-                        IGBaseBlock block = IGRegistryGrabber.grabBlock(MaterialUseType.ROUGH_BRICKS, resourceItem.getMaterial());
-                        ShapedRecipeBuilder.shapedRecipe(block)
-                                .patternLine("xx")
-                                .patternLine("xx")
-                                .key('x', item)
-                                //.addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE)) recipe unlock criterion
-                                .build(consumer);
-                    }
-                    if(resourceItem.subtype.equals(MaterialUseType.POLISHED_CHUNK))
-                    {
-                        IGBaseBlock block = IGRegistryGrabber.grabBlock(MaterialUseType.NORMAL_BRICKS, resourceItem.getMaterial());
-                        ShapedRecipeBuilder.shapedRecipe(block)
-                                .patternLine("xx")
-                                .patternLine("xx")
-                                .key('x', item)
-                                //.addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(Blocks.COBBLESTONE)) recipe unlock criterion
-                                .build(consumer);
-                    }
-                }
-            } catch(Exception e)
-            {
-                System.out.println("Failed to create Recipe: " + e);
-            }
-        }
-    }
+	public void createBasicRecipes(Consumer<IFinishedRecipe> consumer)
+	{
+		for(Item item : IGContent.registeredIGItems.values())
+		{
+			try
+			{
+				if(item instanceof IGMaterialResourceItem)
+				{
+					IGMaterialResourceItem resourceItem = (IGMaterialResourceItem)item;
+					if(resourceItem.subtype.equals(MaterialUseType.CHUNK))
+					{
+						IGBaseBlock block = IGRegistryGrabber.grabBlock(MaterialUseType.ROUGH_BRICKS, resourceItem.getMaterial());
+						ShapedRecipeBuilder.shapedRecipe(block)
+								.patternLine("xx")
+								.patternLine("xx")
+								.key('x', item)
+								.setGroup("bricks")
+								.addCriterion("has_chunk", InventoryChangeTrigger.Instance.forItems(item))
+								.build(consumer);
+					}
+					if(resourceItem.subtype.equals(MaterialUseType.POLISHED_CHUNK))
+					{
+						IGBaseBlock block = IGRegistryGrabber.grabBlock(MaterialUseType.NORMAL_BRICKS, resourceItem.getMaterial());
+						ShapedRecipeBuilder.shapedRecipe(block)
+								.patternLine("xx")
+								.patternLine("xx")
+								.key('x', item)
+								.setGroup("bricks")
+								.addCriterion("has_chunk", InventoryChangeTrigger.Instance.forItems(item))
+								.build(consumer);
+					}
+				}
+			} catch(Exception e)
+			{
+				System.out.println("Failed to create Recipe: "+e);
+			}
+		}
+	}
 }
