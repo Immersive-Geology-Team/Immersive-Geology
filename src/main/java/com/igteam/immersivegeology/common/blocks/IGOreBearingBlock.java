36af8b7db2641b9332d3b02875599b08ccb45ef3
Muddykat <JarrodMuddyman@gmail.com>
2020-09-28 19:13:20 +1000

    Fixed Block Drops

--- src/main/java/com/igteam/immersivegeology/common/blocks/IGOreBearingBlock.java
+++ src/main/java/com/igteam/immersivegeology/common/blocks/IGOreBearingBlock.java
@@ -70,7 +70,7 @@ public class IGOreBearingBlock extends IGMaterialBlock implements IIGOreBlock
 		ItemStack tool = player.getItemStackFromSlot(EquipmentSlotType.MAINHAND);
 		if(!player.isCreative()&&!player.isSpectator()&&this.canHarvestBlock(state, worldIn, pos, player))
 		{
-			if(subtype==MaterialUseType.ORE_BEARING&&!tool.getToolTypes().contains(ToolType.PICKAXE))
+			if(subtype==MaterialUseType.ORE_BEARING&&tool.getToolTypes().contains(ToolType.PICKAXE))
 			{
 				boolean silk = EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, tool) > 0;
 				if(!silk)
@@ -105,8 +105,6 @@ public class IGOreBearingBlock extends IGMaterialBlock implements IIGOreBlock
 				worldIn.addEntity(new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(this.itemBlock)));
 			}
 		}
-
-		super.onBlockHarvested(worldIn, pos, state, player);
 	}
 
 	public Material getOreMaterial()
