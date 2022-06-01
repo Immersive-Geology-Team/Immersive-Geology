2b76de36ec2a300483cba45059a7e1373f387e69
UnSchtalch <shadow.93.04@gmail.com>
2022-06-01 18:36:38 +0300

    More Data again

---
+++ src/api/java/igteam/immersive_geology/materials/data/mineral/variants/MaterialMineralUraninite.java
@@ -0,0 +1,57 @@
+package igteam.immersive_geology.materials.data.mineral.variants;
+
+import igteam.immersive_geology.materials.data.mineral.MaterialBaseMineral;
+import igteam.immersive_geology.materials.helper.CrystalFamily;
+import igteam.immersive_geology.materials.helper.PeriodicTableElement;
+import igteam.immersive_geology.materials.pattern.MaterialPattern;
+import igteam.immersive_geology.processing.IGProcessingStage;
+import net.minecraft.item.Rarity;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class  MaterialMineralUraninite extends MaterialBaseMineral {
+
+    public  MaterialMineralUraninite() {
+        super("uraninite");
+    }
+
+    @Override
+    public int getColor(MaterialPattern p) {
+        return 0xB2BEB5;
+    }
+
+    @Override
+    public CrystalFamily getCrystalFamily() {
+        return CrystalFamily.CUBIC;
+    }
+
+    @Override
+    protected void setupProcessingStages() {
+        super.setupProcessingStages();
+
+        new IGProcessingStage(this,"Extraction Stage") {
+            @Override
+            protected void describe() {
+
+            }
+        };
+    }
+
+    @Override
+    public LinkedHashSet<PeriodicTableElement.ElementProportion> getElements()
+    {
+        // TODO Auto-generated method stub
+        return new LinkedHashSet<>(Arrays.asList(
+                new PeriodicTableElement.ElementProportion(PeriodicTableElement.URANIUM),
+                new PeriodicTableElement.ElementProportion(PeriodicTableElement.OXYGEN, 2)
+        ));
+    }
+
+    @Override
+    public Rarity getRarity()
+    {
+        return Rarity.UNCOMMON;
+    }
+
+}
