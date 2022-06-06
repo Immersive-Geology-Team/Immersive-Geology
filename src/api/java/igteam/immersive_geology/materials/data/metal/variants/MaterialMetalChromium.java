6991497244017db44c843a0b444ab1bbf3d49d1b
UnSchtalch <shadow.93.04@gmail.com>
2022-06-06 11:37:09 +0300

    Texture update by SteelBlue8 Hardcode (FIXME) to load colored textures by me

--- src/api/java/igteam/immersive_geology/materials/data/metal/variants/MaterialMetalChromium.java
+++ src/api/java/igteam/immersive_geology/materials/data/metal/variants/MaterialMetalChromium.java
@@ -1,9 +1,13 @@
 package igteam.immersive_geology.materials.data.metal.variants;
 
+import igteam.immersive_geology.IGApi;
 import igteam.immersive_geology.materials.data.metal.MaterialBaseMetal;
 import igteam.immersive_geology.materials.helper.PeriodicTableElement;
 import igteam.immersive_geology.materials.helper.PeriodicTableElement.ElementProportion;
+import igteam.immersive_geology.materials.pattern.BlockPattern;
+import igteam.immersive_geology.materials.pattern.ItemPattern;
 import igteam.immersive_geology.materials.pattern.MaterialPattern;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
@@ -16,7 +20,38 @@ public class MaterialMetalChromium extends MaterialBaseMetal {
 
     @Override
     public int getColor(MaterialPattern p) {
+        if ((p == ItemPattern.ingot) || (p == ItemPattern.nugget) || (p == ItemPattern.plate)
+                || (p == ItemPattern.block_item) || (p == BlockPattern.storage))
+            return 0xFFFFFF;
         return 0xD7B4F3;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(MaterialPattern pattern) {
+        if (pattern instanceof BlockPattern)
+        {
+            BlockPattern i = (BlockPattern) pattern;
+            switch (i)
+            {
+                case storage: return new ResourceLocation(IGApi.MODID, "block/colored/chromium/storage");
+
+                //case sheetmetal:return new ResourceLocation(IGApi.MODID, "block/colored/chromium/sheetmetal");
+            }
+        }
+        if (pattern instanceof ItemPattern)
+        {
+            ItemPattern i = (ItemPattern) pattern;
+            switch (i)
+            {
+                case ingot:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/chromium/ingot");
+                case nugget:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/chromium/nugget");
+                case plate:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/chromium/plate");
+            }
+        }
+        return super.getTextureLocation(pattern);
     }
 
     @Override
--- src/api/java/igteam/immersive_geology/materials/data/metal/variants/MaterialMetalPlatinum.java
+++ src/api/java/igteam/immersive_geology/materials/data/metal/variants/MaterialMetalPlatinum.java
@@ -1,9 +1,12 @@
 package igteam.immersive_geology.materials.data.metal.variants;
 
+import igteam.immersive_geology.IGApi;
 import igteam.immersive_geology.materials.data.metal.MaterialBaseMetal;
 import igteam.immersive_geology.materials.helper.PeriodicTableElement;
+import igteam.immersive_geology.materials.pattern.BlockPattern;
 import igteam.immersive_geology.materials.pattern.ItemPattern;
 import igteam.immersive_geology.materials.pattern.MaterialPattern;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
@@ -17,12 +20,44 @@ public class MaterialMetalPlatinum extends MaterialBaseMetal {
 
     @Override
     public int getColor(MaterialPattern p) {
+
+        if ((p == ItemPattern.ingot) || (p == ItemPattern.nugget) || (p == ItemPattern.plate)
+                || (p == ItemPattern.block_item) || (p == BlockPattern.storage))
+            return 0xFFFFFF;
         return p == ItemPattern.metal_oxide ? 0x65698C : 0xe7e7f7;
     }
 
     @Override
     public boolean isNative() {
         return true;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(MaterialPattern pattern) {
+        if (pattern instanceof BlockPattern)
+        {
+            BlockPattern i = (BlockPattern) pattern;
+            switch (i)
+            {
+                case storage: return new ResourceLocation(IGApi.MODID, "block/colored/platinum/storage");
+
+                //case sheetmetal:return new ResourceLocation(IGApi.MODID, "block/colored/platinum/sheetmetal");
+            }
+        }
+        if (pattern instanceof ItemPattern)
+        {
+            ItemPattern i = (ItemPattern) pattern;
+            switch (i)
+            {
+                case ingot:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/platinum/ingot");
+                case nugget:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/platinum/nugget");
+                case plate:
+                    return new ResourceLocation(IGApi.MODID, "item/colored/platinum/plate");
+            }
+        }
+        return super.getTextureLocation(pattern);
     }
 
     @Override
