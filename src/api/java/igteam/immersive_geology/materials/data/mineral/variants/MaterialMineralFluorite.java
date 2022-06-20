f58bb220fe0966ef3e642eb622b82cceb50c4180
UnSchtalch <shadow.93.04@gmail.com>
2022-06-20 14:22:33 +0300

    Fluorite processing

--- src/api/java/igteam/immersive_geology/materials/data/mineral/variants/MaterialMineralFluorite.java
+++ src/api/java/igteam/immersive_geology/materials/data/mineral/variants/MaterialMineralFluorite.java
@@ -1,13 +1,22 @@
 package igteam.immersive_geology.materials.data.mineral.variants;
 
+import blusunrize.immersiveengineering.api.crafting.FluidTagInput;
 import igteam.immersive_geology.materials.FluidEnum;
 import igteam.immersive_geology.materials.MetalEnum;
+import igteam.immersive_geology.materials.MineralEnum;
+import igteam.immersive_geology.materials.SlurryEnum;
 import igteam.immersive_geology.materials.data.mineral.MaterialBaseMineral;
 import igteam.immersive_geology.materials.helper.CrystalFamily;
 import igteam.immersive_geology.materials.helper.MaterialInterface;
 import igteam.immersive_geology.materials.helper.PeriodicTableElement;
+import igteam.immersive_geology.materials.pattern.FluidPattern;
+import igteam.immersive_geology.materials.pattern.ItemPattern;
 import igteam.immersive_geology.processing.IGProcessingStage;
+import igteam.immersive_geology.processing.helper.IGStageDesignation;
+import igteam.immersive_geology.processing.helper.IRecipeBuilder;
 import net.minecraft.item.Rarity;
+import net.minecraft.tags.FluidTags;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -30,10 +39,23 @@ public class MaterialMineralFluorite extends MaterialBaseMineral {
     protected void setupProcessingStages() {
         super.setupProcessingStages();
 
-        new IGProcessingStage(this,"Extraction Stage") {
+        new IGProcessingStage(this, IGStageDesignation.synthesis) {
             @Override
             protected void describe() {
+                IRecipeBuilder.crushing(this).create(
+                        "crushed_ore_" + getName() + "_to_dust",
+                        getItemTag(ItemPattern.crushed_ore),
+                        getStack(ItemPattern.dust),
+                        10000, 200);
 
+                IRecipeBuilder.chemical(this).create(
+                        "dust_" + getName() + "_to_acid",
+                        getStack(ItemPattern.dust),
+                        new FluidTagInput(FluidEnum.SulfuricAcid.getFluidTag(FluidPattern.fluid), 125),
+                        new FluidTagInput(FluidTags.WATER, 250),
+                        MineralEnum.Gypsum.getStack(ItemPattern.dust),
+                        new FluidStack(FluidEnum.HydrofluoricAcid.getFluid(FluidPattern.fluid), 125),
+                        120, 10000);
             }
         };
     }
