c858142ba2c86a881233bdab67e44bbba2303064
UnSchtalch <shadow.93.04@gmail.com>
2022-06-20 11:40:26 +0300

    Sphalerite and Smithsonite processing

--- src/api/java/igteam/immersive_geology/materials/data/mineral/variants/MaterialMineralSphalerite.java
+++ src/api/java/igteam/immersive_geology/materials/data/mineral/variants/MaterialMineralSphalerite.java
@@ -1,13 +1,22 @@
 package igteam.immersive_geology.materials.data.mineral.variants;
 
+import blusunrize.immersiveengineering.api.crafting.FluidTagInput;
 import igteam.immersive_geology.materials.FluidEnum;
 import igteam.immersive_geology.materials.MetalEnum;
+import igteam.immersive_geology.materials.SlurryEnum;
 import igteam.immersive_geology.materials.data.mineral.MaterialBaseMineral;
 import igteam.immersive_geology.materials.helper.CrystalFamily;
 import igteam.immersive_geology.materials.helper.MaterialInterface;
 import igteam.immersive_geology.materials.helper.PeriodicTableElement;
+import igteam.immersive_geology.materials.pattern.FluidPattern;
+import igteam.immersive_geology.materials.pattern.ItemPattern;
+import igteam.immersive_geology.materials.pattern.MaterialPattern;
 import igteam.immersive_geology.processing.IGProcessingStage;
+import igteam.immersive_geology.processing.helper.IRecipeBuilder;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
@@ -26,13 +35,52 @@ public class MaterialMineralSphalerite extends MaterialBaseMineral {
     }
 
     @Override
+    public boolean hasSlag(){
+        return true;
+    }
+
+    @Override
     protected void setupProcessingStages() {
         super.setupProcessingStages();
 
-        new IGProcessingStage(this,"Extraction Stage") {
+        new IGProcessingStage(this,"Refining Stage") {
             @Override
             protected void describe() {
+                IRecipeBuilder.roast(this).create("mineral_" + getName() + "_to_slag",
+                        getParentMaterial().getStack(ItemPattern.crushed_ore), getParentMaterial().getStack(ItemPattern.slag),1000, 1);
+                IRecipeBuilder.crushing(this).create("slag_" + getName() + "_to_dust",
+                        getItemTag(ItemPattern.slag),
+                        getStack(ItemPattern.dust), 10000, 100);
+                IRecipeBuilder.separating(this).create(
+                        getItemTag(ItemPattern.slag),
+                        MetalEnum.Iron.getStack(ItemPattern.metal_oxide),
+                        MetalEnum.Zinc.getStack(ItemPattern.metal_oxide));
 
+                IRecipeBuilder.blasting(this).create(
+                        "oxide_" + MetalEnum.Iron.getName() + "_to_ingot",
+                        MetalEnum.Iron.getItemTag(ItemPattern.metal_oxide),
+                        MetalEnum.Iron.getStack(ItemPattern.ingot));
+            }
+        };
+
+        new IGProcessingStage(this,"Purification Stage") {
+            @Override
+            protected void describe() {
+                IRecipeBuilder.chemical(this).create(
+                        "metal_oxide_" + MetalEnum.Zinc.getName() + "_to_slurry",
+                        MetalEnum.Zinc.getStack(ItemPattern.metal_oxide),
+                        new FluidTagInput(FluidEnum.SulfuricAcid.getFluidTag(FluidPattern.fluid), 250),
+                        new FluidTagInput(FluidTags.WATER, 250),
+                        ItemStack.EMPTY,
+                        SlurryEnum.ZINC.getType(FluidEnum.SulfuricAcid).getFluidStack(FluidPattern.slurry, 250),
+                        120, 10000);
+
+
+                IRecipeBuilder.crystalize(this).create(
+                        "slurry_" + SlurryEnum.ZINC.getType(FluidEnum.SulfuricAcid).getName() + "_to_crystal",
+                        MetalEnum.Zinc.getStack(ItemPattern.crystal),
+                        new FluidTagInput(SlurryEnum.ZINC.getType(FluidEnum.SulfuricAcid).getFluidTag(FluidPattern.slurry), 250),
+                        300,12000);
             }
         };
     }
