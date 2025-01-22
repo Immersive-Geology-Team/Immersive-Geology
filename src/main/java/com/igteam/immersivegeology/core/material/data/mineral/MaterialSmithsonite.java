0314c397fc9e7b06c52735b1573673fcda4e798d
UnSchtalch <shadow.93.04@gmail.com>
2025-01-22 11:00:45 +0300

    In-depth Smithsonite processing chain

--- src/main/java/com/igteam/immersivegeology/core/material/data/mineral/MaterialSmithsonite.java
+++ src/main/java/com/igteam/immersivegeology/core/material/data/mineral/MaterialSmithsonite.java
@@ -1,5 +1,6 @@
 package com.igteam.immersivegeology.core.material.data.mineral;
 
+import blusunrize.immersiveengineering.common.register.IEItems.Ingredients;
 import com.igteam.immersivegeology.core.material.data.enums.MetalEnum;
 import com.igteam.immersivegeology.core.material.data.types.MaterialMineral;
 import com.igteam.immersivegeology.core.material.helper.flags.IFlagType;
@@ -9,6 +10,7 @@ import com.igteam.immersivegeology.core.material.helper.material.MaterialInterfa
 import com.igteam.immersivegeology.core.material.helper.material.StoneFormation;
 import com.igteam.immersivegeology.core.material.helper.material.recipe.IGStageDesignation;
 import com.igteam.immersivegeology.core.material.helper.material.recipe.helper.IGMethodBuilder;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.Tags.Biomes;
 
 import java.util.LinkedHashSet;
@@ -23,7 +25,7 @@ public class MaterialSmithsonite extends MaterialMineral {
         super();
         this.acceptableStoneTypes.add(StoneFormation.SEDIMENTARY);
         this.acceptableStoneTypes.add(StoneFormation.MINECRAFT_STONE);
-
+        addFlags(ItemCategoryFlags.PELLET);
         CONFIG = new MineralConfig(8,50,2,30,70,550,0.5,false,Optional.of(Biomes.IS_HOT));
     }
 
@@ -46,8 +48,26 @@ public class MaterialSmithsonite extends MaterialMineral {
     @Override
     public void setupRecipeStages()
     {
-        IGMethodBuilder.decompose(this, IGStageDesignation.PREPARATION).create(ItemCategoryFlags.CRUSHED_ORE,1,
-                ItemCategoryFlags.METAL_OXIDE, MetalEnum.Zinc.instance(), 1, 300, 153600);
+        IGMethodBuilder.decompose(this, IGStageDesignation.PREPARATION).create(ItemCategoryFlags.SLAG,
+                ItemCategoryFlags.CRUSHED_ORE, 1,300, 153600);
+
+        IGMethodBuilder.pulverization(this, IGStageDesignation.PREPARATION).create(ItemCategoryFlags.SLAG,
+                ItemCategoryFlags.POWDERED_SLAG);
+
+        //It has blue tint, so copper is present
+        IGMethodBuilder.separating(this, IGStageDesignation.PREPARATION).create(getItemTag(ItemCategoryFlags.POWDERED_SLAG),
+                MetalEnum.Zinc.getStack(ItemCategoryFlags.METAL_OXIDE),
+                MetalEnum.Copper.getStack(ItemCategoryFlags.METAL_OXIDE),
+                0.075f, 200, 1000);
+
+        //No byproducts and zinc is evaporated
+        IGMethodBuilder.blasting(this, IGStageDesignation.EXTRACTION).create(this, ItemCategoryFlags.SLAG,
+                getProductionMaterial().instance(),ItemCategoryFlags.INGOT, new ItemStack(Ingredients.SLAG), 900);
+
+        //No byproducts and most of the zinc is evaporated
+        IGMethodBuilder.blasting(this, IGStageDesignation.EXTRACTION).create("crushed_ore_"+getName()+"_to_ingot",
+                getItemTag(ItemCategoryFlags.CRUSHED_ORE),
+                getProductionMaterial().getStack(ItemCategoryFlags.NUGGET, 5));
 
     }
 }
