20bf9cc44617407c2c59aa01d2504695df3b292f
UnSchtalch <shadow.93.04@gmail.com>
2025-02-06 12:59:07 +0300

    PGM Extraction

--- src/main/java/com/igteam/immersivegeology/core/material/data/metal/MaterialPlatinum.java
+++ src/main/java/com/igteam/immersivegeology/core/material/data/metal/MaterialPlatinum.java
@@ -8,11 +8,20 @@
 
 package com.igteam.immersivegeology.core.material.data.metal;
 
+import blusunrize.immersiveengineering.api.crafting.FluidTagInput;
+import blusunrize.immersiveengineering.api.crafting.IngredientWithSize;
 import com.igteam.immersivegeology.common.world.features.helper.IGGenerationType;
+import com.igteam.immersivegeology.core.lib.IGLib;
+import com.igteam.immersivegeology.core.material.data.enums.ChemicalEnum;
+import com.igteam.immersivegeology.core.material.data.enums.MetalEnum;
 import com.igteam.immersivegeology.core.material.data.types.MaterialMineral;
 import com.igteam.immersivegeology.core.material.data.types.MaterialNativeMetal;
+import com.igteam.immersivegeology.core.material.helper.flags.BlockCategoryFlags;
 import com.igteam.immersivegeology.core.material.helper.flags.IFlagType;
+import com.igteam.immersivegeology.core.material.helper.flags.ItemCategoryFlags;
 import com.igteam.immersivegeology.core.material.helper.material.StoneFormation;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.IGStageDesignation;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.helper.IGMethodBuilder;
 import net.minecraftforge.common.Tags.Biomes;
 
 import java.util.Optional;
@@ -31,5 +40,19 @@ public class MaterialPlatinum extends MaterialNativeMetal {
     @Override
     protected BiFunction<IFlagType<?>, Integer, Integer> materialColorFunction() {
         return ((p, i) -> (0xe7e7f7));
+    }
+
+    @Override
+    public void setupRecipeStages()
+    {
+        super.setupRecipeStages();
+        IGMethodBuilder.chemical(this, IGStageDesignation.LEECHING).create(
+                ItemCategoryFlags.COMPOUND_DUST, BlockCategoryFlags.FLUID,
+                MetalEnum.Platinum.getStack(ItemCategoryFlags.GRIT, 1),
+                ChemicalEnum.ChemicalWaste.getFluidStack(IGLib.SLURRY_FROM_ACID_AMOUNT),
+                IngredientWithSize.of(getStack(ItemCategoryFlags.COMPOUND_DUST, 1)),
+                new FluidTagInput(ChemicalEnum.Ammonia.getFluidTag(BlockCategoryFlags.FLUID), 125),
+                new FluidTagInput(ChemicalEnum.HydrochloricAcid.getFluidTag(BlockCategoryFlags.FLUID), 125),
+                new FluidTagInput(ChemicalEnum.NitricAcid.getFluidTag(BlockCategoryFlags.FLUID), 125), 200, 51200);
     }
 }
