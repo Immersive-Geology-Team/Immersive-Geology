26513044b4e396760e3e12d6b6ee67018a3c9173
NightmareTwilight <casper.areskoug@icloud.com>
2025-04-11 10:18:34 +0200

    Remove the HEftyWrench class, isn't needed at this time.
    Signed-off-by: NightmareTwilight <casper.areskoug@icloud.com>

--- src/gametest/java/com/igteam/immersivegeology/gametest/ImmersiveGeology.java
+++ src/gametest/java/com/igteam/immersivegeology/gametest/ImmersiveGeology.java
@@ -7,42 +7,14 @@
  */
 
 package com.igteam.immersivegeology.gametest;
-import blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler;
-import blusunrize.immersiveengineering.api.multiblocks.MultiblockHandler.IMultiblock;
-import blusunrize.immersiveengineering.api.multiblocks.TemplateMultiblock;
-import blusunrize.immersiveengineering.common.register.IEItems.Tools;
-import com.igteam.immersivegeology.common.block.multiblocks.IGTemplateMultiblock;
-import com.igteam.immersivegeology.common.item.IGHeftyWrenchItem;
-import com.igteam.immersivegeology.common.item.IGMBFormationItem;
 import com.igteam.immersivegeology.core.lib.IGLib;
-import com.igteam.immersivegeology.core.material.data.enums.MetalEnum;
-import com.igteam.immersivegeology.core.material.data.enums.StoneEnum;
-import com.igteam.immersivegeology.core.material.helper.flags.ItemCategoryFlags;
 import com.igteam.immersivegeology.gametest.tests.ClientTests;
 import com.igteam.immersivegeology.gametest.tests.CommonTests;
 import com.igteam.immersivegeology.gametest.tests.ServerTests;
-import net.minecraft.Util;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.*;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.StructureBlockInfo;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.gametest.GameTestHolder;
-import net.minecraftforge.gametest.PrefixGameTestTemplate;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @GameTestHolder(IGLib.MODID)
