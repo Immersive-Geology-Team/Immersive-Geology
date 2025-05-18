34c4e82111599ebc8fa7c9dcb4f4a2a7b7e689d1
UnSchtalch <shadow.93.04@gmail.com>
2025-05-18 13:57:33 +0300

    Phosphoric Acid production and lang file fixed for it

--- src/main/java/com/igteam/immersivegeology/core/material/data/mineral/MaterialApatite.java
+++ src/main/java/com/igteam/immersivegeology/core/material/data/mineral/MaterialApatite.java
@@ -8,20 +8,34 @@
 
 package com.igteam.immersivegeology.core.material.data.mineral;
 
+import blusunrize.immersiveengineering.api.crafting.FluidTagInput;
+import blusunrize.immersiveengineering.api.crafting.IngredientWithSize;
 import com.igteam.immersivegeology.common.world.features.helper.noise.IGGenerationType;
+import com.igteam.immersivegeology.core.lib.IGLib;
+import com.igteam.immersivegeology.core.material.data.enums.ChemicalEnum;
 import com.igteam.immersivegeology.core.material.data.enums.MetalEnum;
 import com.igteam.immersivegeology.core.material.data.types.MaterialSulphideMineral;
+import com.igteam.immersivegeology.core.material.helper.flags.BlockCategoryFlags;
 import com.igteam.immersivegeology.core.material.helper.flags.IFlagType;
+import com.igteam.immersivegeology.core.material.helper.flags.ItemCategoryFlags;
 import com.igteam.immersivegeology.core.material.helper.material.CrystalFamily;
 import com.igteam.immersivegeology.core.material.helper.material.MaterialInterface;
 import com.igteam.immersivegeology.core.material.helper.material.StoneFormation;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.IGStageDesignation;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.helper.IGMethodBuilder;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.helper.IGRecipeChain;
+import com.igteam.immersivegeology.core.material.helper.material.recipe.helper.IGRecipeNode;
 import net.minecraftforge.common.Tags.Biomes;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.function.BiFunction;
 
 public class MaterialApatite extends MaterialSulphideMineral {
+
+    protected IGRecipeChain acid_production = new IGRecipeChain(this, "Phosphoric Acid Production", 0);
+
 
     public MaterialApatite() {
         super();
@@ -54,6 +68,25 @@ public class MaterialApatite extends MaterialSulphideMineral {
     public void setupRecipeStages()
     {
         super.setupRecipeStages();
+
+        IGRecipeNode prep = IGMethodBuilder.crushing(this, IGStageDesignation.EXTRACTION)
+                .create("crushed_ore" + getName() + "_to_dust", getStack(ItemCategoryFlags.CRUSHED_ORE, 1),
+                        getStack(ItemCategoryFlags.GRIT, 1), 10000, 100).addToTree(acid_production);
+
+        IGRecipeNode grit = IGMethodBuilder.crushing(this, IGStageDesignation.PREPARATION).create(ItemCategoryFlags.CRUSHED_ORE,
+                ItemCategoryFlags.GRIT, 6000, 100).addToTree(acid_production, prep);
+
+        IGRecipeNode powder_b = IGMethodBuilder.pulverization(this, IGStageDesignation.PREPARATION)
+                .create(ItemCategoryFlags.CRUSHED_ORE, ItemCategoryFlags.POWDER).addToTree(acid_production, prep);
+        IGRecipeNode powder_a = IGMethodBuilder.pulverization(this, IGStageDesignation.PREPARATION)
+                .create(ItemCategoryFlags.GRIT, ItemCategoryFlags.POWDER, 200, 16000).addToTree(acid_production, grit);
+
+        IGMethodBuilder.chemical(this, IGStageDesignation.LEECHING)
+                .create("apatite_dust_to_acid", MetalEnum.Calcium.getStack(ItemCategoryFlags.COMPOUND_DUST),
+                        ChemicalEnum.PhosphoricAcid.getFluidStack(IGLib.ACID_RECOVERED_FROM_SLURRY),
+                        new IngredientWithSize(getItemTag(ItemCategoryFlags.POWDER), IGLib.COMPOUND_FROM_ACID_AMOUNT),
+                        new FluidTagInput(ChemicalEnum.SulfuricAcid.getFluidTag(BlockCategoryFlags.FLUID), IGLib.ACID_TO_COMPOUND_AMOUNT),
+                        null, null, 200, 51200).joinBranches(acid_production, powder_a, powder_b);
     }
 
     @Override
