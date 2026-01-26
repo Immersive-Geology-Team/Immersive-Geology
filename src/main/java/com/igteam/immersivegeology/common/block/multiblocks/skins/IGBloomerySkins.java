077c97f3f3f80db8055fcb6396c426de0a8dc7c5
muddykat <jarrod.muddyman@muddykat.tech>
2026-01-26 19:22:24 +1100

    updated ccyrax's multiblock skin, fixed issues #204 #192 and #196

--- src/main/java/com/igteam/immersivegeology/common/block/multiblocks/skins/IGBloomerySkins.java
+++ src/main/java/com/igteam/immersivegeology/common/block/multiblocks/skins/IGBloomerySkins.java
@@ -18,18 +18,27 @@ import java.util.Locale;
 public enum IGBloomerySkins implements IIGMultiSkinHelper
 {
 	DEFAULT("default", ChatFormatting.GOLD, SkinCreditType.PETER, "peter"),
-	LEGACY("legacy", ChatFormatting.UNDERLINE, SkinCreditType.DEVELOPER, "muddykat");
+	LEGACY("legacy", ChatFormatting.UNDERLINE, SkinCreditType.DEVELOPER, "muddykat"),
+	INDUSTRIAL("industrial", ChatFormatting.LIGHT_PURPLE, SkinCreditType.SUPPORTER, "ccyax", true);
 
 	private final String skin;
 	private final SkinCreditType type;
 	private final ChatFormatting formatting;
 	private final String credit;
+	private final boolean altModel;
+
 	IGBloomerySkins(String s, ChatFormatting formatting, SkinCreditType type, String credit)
+	{
+		this(s,formatting,type,credit, false);
+	}
+
+	IGBloomerySkins(String s, ChatFormatting formatting, SkinCreditType type, String credit, boolean altModel)
 	{
 		this.skin = s;
 		this.formatting = formatting;
 		this.type = type;
 		this.credit = credit;
+		this.altModel = altModel;
 	}
 
 	@Override public String getSkin() { return skin; }
@@ -63,12 +72,10 @@ public enum IGBloomerySkins implements IIGMultiSkinHelper
 	@Override
 	public boolean alternativeModel()
 	{
-		return false;
+		return altModel;
 	}
 
 	@Override
-	public Enum<?> instance()
-	{
-		return this;
-	}
+	public Enum<?> instance() { return this; }
+
 }
